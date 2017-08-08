1fc74bee81d2a5c79abb9a936bf990284d3f3878
mathu97 <mathusans52@gmail.com>
2017-08-08 13:52:50 -0400

    Adding Basic UI Design

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by CLQA on 2017-08-08.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
