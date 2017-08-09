9d65c37c086546c7c5f30da8b106f1aa93e3df08
Mathusan Selvarajah <mathusans52@gmail.com>
2017-08-08 23:41:49 -0400

    Finishing up UI changes

---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,14 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Mathusan Selvarajah on 2017-08-08.
+//  Copyright © 2017 Mathusan. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    
+}
