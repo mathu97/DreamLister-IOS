a920d353106461e5fe0ee17cbfda804f1292818e
Mathusan Selvarajah <mathusans52@gmail.com>
2017-09-28 00:10:17 -0400

    Changing nav bar back button and removing the label

--- DreamLister/ItemDetailVC.swift
+++ DreamLister/ItemDetailVC.swift
@@ -12,24 +12,17 @@ class ItemDetailVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Removing previous text on navigation bar, now will only contain the left chevron to go back
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
